e9e92d09b0404996bf85134ad62676c8789b55fa
Hacker-free <112991346+Hacker-free@users.noreply.github.com>
2022-09-07 01:00:53 +0100

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,70 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+print ('\033[1;32m')
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('pkg install sl -y')
+os.system ('clear')
+print ("\033[1;36m")
+os.system ('figlet Fanan Fx9')
+def main (main):
+    print ("")
+    time.sleep(1)
+    print ("")
+    time.sleep(0.2)
+    print ('   \033[1;33m{\033[1;36m1\033[1;33m}\033[1;32m ﺮﯾﺎﻓ يﺮﻓ تﺎﺑﺎﺴﺤ قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m2\033[1;33m}\033[1;32m كﻮﺒﺴﻴﻓ قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m3\033[1;33m}\033[1;32m ماﺮﭴﺘﺴﻧﺍ قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m4\033[1;33m}\033[1;32m بﺎﺴﺗو قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;33m{\033[1;36m5\033[1;33m}\033[1;32m wifi قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;30m{\033[1;30m?\033[1;30m} \033[1;32mآَﺒﯾﺮﻗ\033[1;30m  ,pubg mobile,pes 2022, fifa 22 تﺎﺑﺎﺴﺤ قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;30m{\033[1;30m6\033[1;30m} \033[1;32mآَﺒﯾﺮﻗ\033[1;30m +9 ,Gmail, Hotmail , Yahoo تﺎﺑﺎﺴﺤ قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    time.sleep(1)
+    print ('   \033[1;30m{\033[1;30m7\033[1;30m} \033[1;32mآَﺒﯾﺮﻗ\033[1;30m Android 7,8,9,10,11,12 قاﺮﺘﺧإ')
+    time.sleep(1)
+    print ("")
+    print ('       \033[1;32m 1,2,3,4,5,\033[1;30m6,7, \033[1;35mﻦﻣ مﻗﺮﻟﺍ ﺮﺘﺧإ')
+    time.sleep(1)
+    v7x2 = input ("   \033[1;33mمﻗر بﺘڪأ \033[1;35m ==> \033[1;32m")
+    if v7x2 == '1' :
+             os.system('python 1.py')
+             restart () 
+    elif v7x2 == '2' : 
+             os.system ('python 2.py')
+    elif v7x2 == '3' :
+             os.system ('python 3.py')
+    elif v7x2 == '4' :
+             os.system ('python 4.py')
+    elif v7x2 == '5' :
+            os.system ('python 5.py')
+    elif v7x2 == '6' :
+            os.system ('python 6.py')
+    elif v7x2 == '7' :
+            os.system ('python 7.py')
+main (main)
+home_main ()
